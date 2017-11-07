96eb188dfe0e4ee798638cf3f6fe91fcc48cb1f1
alexaruiz <ruiza20@up.edu>
2017-11-06 18:02:26 -0800

    added FHumanPlayer, FMainActivity, and FTitleSreen ( I think we will need this for the configuration) changed to match our design document

---
+++ app/src/main/java/edu/up/cs301/flinch/FStateElements/FMainActivity.java
@@ -0,0 +1,85 @@
+package edu.up.cs301.flinch.FStateElements;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+import edu.up.cs301.flinch.FComputerPlayer;
+import edu.up.cs301.flinch.SJLocalGame;
+import edu.up.cs301.game.GameMainActivity;
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.LocalGame;
+import edu.up.cs301.game.config.GameConfig;
+import edu.up.cs301.game.config.GamePlayerType;
+
+/**
+ * Created by alexaruiz on 11/6/17.
+ */
+
+public class FMainActivity extends GameMainActivity {
+    public static final int PORT_NUMBER = 4752;
+
+    public GameConfig createDefaultConfig() {
+
+        // Define the allowed player types
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+
+        playerTypes.add(new GamePlayerType("human player (green)") {
+            public GamePlayer createPlayer(String name) {
+                return new FHumanPlayer(name, Color.GREEN);
+            }});
+        playerTypes.add(new GamePlayerType("human player (yellow)") {
+            public GamePlayer createPlayer(String name) {
+                return new FHumanPlayer(name, Color.YELLOW);
+            }
+        });
+        playerTypes.add(new GamePlayerType("computer player (normal)") {
+            public GamePlayer createPlayer(String name) {
+                return new FComputerPlayer(name);
+            }
+        });
+        playerTypes.add(new GamePlayerType("computer player (fast)") {
+            public GamePlayer createPlayer(String name) {
+                return new FComputerPlayer(name, 0.3);
+            }
+        });
+        playerTypes.add(new GamePlayerType("computer player (slow)") {
+            public GamePlayer createPlayer(String name) {
+                return new FComputerPlayer(name, 1.0);
+            }
+        });
+        playerTypes.add(new GamePlayerType("computer player (very fast)") {
+            public GamePlayer createPlayer(String name) {
+                return new FComputerPlayer(name, 0.15);
+            }
+        });
+        playerTypes.add(new GamePlayerType("computer player (very slow)") {
+            public GamePlayer createPlayer(String name) {
+                return new FComputerPlayer(name, 3.5);
+            }
+        });
+
+        // Create a game configuration class for SlapJack
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 2, "Flinch", PORT_NUMBER);
+
+        // Add the default players
+        defaultConfig.addPlayer("Human", 0);
+        defaultConfig.addPlayer("Computer", 2);
+
+        // Set the initial information for the remote player
+        defaultConfig.setRemoteData("Guest", "", 1);
+
+        //done!
+        return defaultConfig;
+    }//createDefaultConfig
+
+    public FTitleScreen createTitleScreen() {
+        return null;
+    }
+
+
+    public LocalGame createLocalGame() {
+        //FLocalGame needs to be created
+        return new FLocalGame();
+    }
+}
---
+++ app/src/main/java/edu/up/cs301/flinch/FStateElements/FTitleScreen.java
@@ -0,0 +1,8 @@
+package edu.up.cs301.flinch.FStateElements;
+
+/**
+ * Created by alexaruiz on 11/6/17.
+ */
+
+class FTitleScreen {
+}
