4a4ebaacd5ba5f1d3016baa29cc2aded2b238064
chelle-plaisted <plaisted20@up.edu>
2017-11-09 22:56:53 -0800

    dumbComputerPlayer maybe working....

--- app/src/main/java/edu/up/cs301/flinch/FSmartComputerPlayer.java
+++ app/src/main/java/edu/up/cs301/flinch/FSmartComputerPlayer.java
@@ -1,5 +1,12 @@
 package edu.up.cs301.flinch;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import edu.up.cs301.cardpile.DiscardPile;
+import edu.up.cs301.cardpile.FlinchPile;
+import edu.up.cs301.cardpile.Hand;
+import edu.up.cs301.flinch.FStateElements.FPlayerState;
 import edu.up.cs301.flinch.FStateElements.FState;
 import edu.up.cs301.game.infoMsg.GameInfo;
 
@@ -28,6 +35,93 @@ public class FSmartComputerPlayer extends FComputerPlayer {
     @Override
     protected void receiveInfo(GameInfo info) {
         super.receiveInfo(info);
+
+        if(this.playerNum == savedState.getWhoseTurn()) {
+            playCards();
+            discard();
+        }
+
+    }
+
+    private void playCards() {
+        boolean cardPlayed;
+        do{
+           cardPlayed = false;
+            if(playFlinch() || playHand() || playDiscard())
+                cardPlayed = true;
+        }while(cardPlayed);
+    }
+
+    private boolean playFlinch() {
+        // look at the flinch pile
+        // if there is a card in the center higher than the flinch pile, hunt to see if we can build to it
+        // if I have no building cards or can play the flinch card--then I'm done hunting
+        // look at the Flinch, then hand, then discards
+        // if there is a non Flinch pile card I can play, add it to the list of toBePlayed cards and call the method recursively with a one lower card goal
+
+        int index = isCardPlayable(me.getTopFlinchCard());
+        if(index != -1) {
+            // I can play the card
+            game.sendAction(new FPlayAction(this, 0, index, new FlinchPile()));
+            return true;
+        }
+        // I could not directly play the card-- can we build to the card?
+        index = potentiallyPlayable();
+
+        if(index != -1) {
+            int targetVal =savedState.getCenterPiles()[index];
+            // look at hand -- can I play the next card
+            playHand();
+        }
+
+
+        return false;
+    }
+
+    private boolean playHand() {
+        // look at the Hand---every card
+        // if I can play the card, play it -- and send index
+        boolean played = false;
+        Hand h = me.getHand();
+        for(int i = 0; i < h.size(); i++) {
+            int index = isCardPlayable(h.getCardAt(i));
+            if(index != -1) {
+                // I can play the card
+                game.sendAction(new FPlayAction(this, index, i, new Hand()));
+                played = true;
+            }
+        }
+        return played;
+    }
+
+    private boolean playDiscard() {
+        // look at the Discard---every card
+        // if I can play the card, play it -- and send index
+        boolean played = false;
+        int[] d = me.getTopDiscards();
+        for(int i = 0; i < d.length; i++) {
+            int index = isCardPlayable(d[i]);
+            if(index != -1) {
+                // I can play the card
+                game.sendAction(new FPlayAction(this, index, i, new DiscardPile()));
+                played = true;
+            }
+        }
+        return played;
+    }
+
+    private int potentiallyPlayable() {
+        int cardVal = me.getTopFlinchCard();
+        int[] centers = savedState.getCenterPiles();
+        int cardDifference = 16;
+        int closest = -1;
+        for(int i = 0; i < centers.length; i++) {
+            if (cardVal - centers[i] < cardDifference && cardVal - centers[i] >= 0) {
+                cardDifference = cardVal - centers[i];
+                closest = i;
+            }
+        }
+        return closest;
     }
 
 }
