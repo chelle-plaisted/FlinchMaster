885c2965f082599bfaeb44a848a7ebe9e4aa3543
WeslynNishimura <nishimuw18@up.edu>
2017-11-15 12:20:23 -0800

    Card class updated with new Card Images and added methods from Dr. Vegdahl's code.

--- app/src/main/java/edu/up/cs301/card/Card.java
+++ app/src/main/java/edu/up/cs301/card/Card.java
@@ -1,18 +1,28 @@
 package edu.up.cs301.card;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
 
 import java.io.Serializable;
+
+import edu.up.cs301.game.R;
 
 /**
  * Card
  *
  * A playing card to be used in the game of Flinch. Unlike a standard deck of 52 cards, Flinch
- * requires a deck of 150 cards numbered 1 through 15.
+ * requires a deck of 150 cards numbered 1 through 15. All card images were made by the Flinch team
+ * and have been placed in the res/drawable-hdpi folder in the project.
+ *
+ * NOTE: In order to display the card-images on the android you need to call the
+ *   Card.initImages(currentActivity)
+ * method during initialization;
  *
  * @author Alexa Ruiz
  * @author Chelle Plaisted
@@ -22,18 +32,17 @@ import java.io.Serializable;
  * @version November 2018
  */
 public class Card implements Serializable {
+
     // to satisfy the Serializable interface
     private static final long serialVersionUID = 893542931190030342L;
 
-    // instance variables
-    private int cardNum; // the card's value (between 1 and 15)
+    // instance variables: the card's rank and the suit
+    private int cardNum;
 
     /**
-     * Card()
+     * Constructor for class card
      *
-     * The Card constructor. Creates an object of Card with the given number.
-     *
-     * @param num the value of the card
+     * @param num the card number
      */
     public Card(int num) {
         // if the number is valid
@@ -46,18 +55,14 @@ public class Card implements Serializable {
     }
 
     /**
-     * Card()
-     *
-     * The Card copy constructor. Create a copy of the given Card object.
+     * Constructor for class card (copy constructor)
      *
      * @param orig the card to copy
      */
     public Card(Card orig) {
-        // set the Card object number to the same as orig
         cardNum = orig.cardNum;
     }
 
-    // Card Methods
     /**
      * getCardNum()
      *
@@ -69,25 +74,110 @@ public class Card implements Serializable {
         // return the value of the card
         return cardNum;
     }
+    /**
+     * Tells whether object are equal -- in other words that they are both Card
+     * objects that represent the same card.
+     *
+     * @return
+     *		true if the two card objects represent the same card, false
+     *		otherwise.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Card)) return false;
+        Card c = (Card)other;
+        return this.cardNum == c.cardNum;
+    }
 
     /**
-     * drawCard()
+     * NOT SURE IF THIS METHOD IS NEEDED*******************************************************
      *
-     * Method to draw the Card object on the GUI
      *
-     * @param g the graphics object on which to draw
-     * @param where a rectangle that tells where the card should be drawn
+     * Tells whether object are equal -- in other words that they are both Card
+     * objects that represent the same card.
+     *
+     * @return
+     *		true if the two card objects represent the same card, false
+     *		otherwise.
      */
-    public void drawCard(Canvas g, RectF where) {
-        // TODO: IMPLEMENT
-        /* DO NOT DELETE
+    //@Override
+    //public int hashCode() {
+        //return cardNum.hashCode()*18737;
+    //}
+
+    /**
+     * Draws the card on a Graphics object.  The card is drawn as a
+     * white card with a black border.  If the card's rank is numerih, the
+     * appropriate number of spots is drawn.  Otherwise the appropriate
+     * picture (e.g., of a queen) is included in the card's drawing.
+     *
+     * @param g  the graphics object on which to draw
+     * @param where  a rectangle that tells where the card should be drawn
+     */
+    public void drawOn(Canvas g, RectF where) {
         // create the paint object
         Paint p = new Paint();
         p.setColor(Color.BLACK);
 
         // get the bitmap for the card
-        // Bitmap bitmap = cardImages[this.getNum().ordinal()];
-        */
+        Bitmap bitmap = cardImages[this.getNum()][0]; //.ordinal()][this.getRank().ordinal()];
+
+        // create the source rectangle
+        Rect r = new Rect(0,0,bitmap.getWidth(),bitmap.getHeight());
+
+        // draw the bitmap into the target rectangle
+        g.drawBitmap(bitmap, r, where, p);
+    }
+
+    // array that contains the android resource indices for the 52 card
+    // images
+    private static int[][] resIdx = {
+            {
+                    R.drawable.Card01_1x, R.drawable.Card02_1x, R.drawable.Card03_1x,
+                    R.drawable.Card04_1x, R.drawable.Card05_1x, R.drawable.Card06_1x,
+                    R.drawable.Card07_1x, R.drawable.Card08_1x, R.drawable.Card09_1x,
+                    R.drawable.Card10_1x, R.drawable.Card11_1x, R.drawable.Card12_1x,
+                    R.drawable.Card13_1x, R.drawable.Card14_1x, R.drawable.Card15_1x,
+            },
+            {
+                    R.drawable.Card01_2x, R.drawable.Card02_2x, R.drawable.Card03_2x,
+                    R.drawable.Card04_2x, R.drawable.Card05_2x, R.drawable.Card06_2x,
+                    R.drawable.Card07_2x, R.drawable.Card08_2x, R.drawable.Card09_2x,
+                    R.drawable.Card10_2x, R.drawable.Card11_2x, R.drawable.Card12_2x,
+                    R.drawable.Card13_2x, R.drawable.Card14_2x, R.drawable.Card15_2x,
+            },
+    };
+
+    // the array of card images
+    private static Bitmap[][] cardImages = null;
+
+    /**
+     * initializes the card images
+     *
+     * @param activity
+     * 		the current activity
+     */
+    public static void initImages(Activity activity) {
+        // if it's already initialized, then ignore
+        if (cardImages != null) return;
+
+        // create the outer array
+        cardImages = new Bitmap[resIdx.length][];
+
+        // loop through the resource-index array, creating a
+        // "parallel" array with the images themselves
+        for (int i = 0; i < resIdx.length; i++) {
+            // create an inner array
+            cardImages[i] = new Bitmap[resIdx[i].length];
+            for (int j = 0; j < resIdx[i].length; j++) {
+                // create the bitmap from the corresponding image
+                // resource, and set the corresponding array element
+                cardImages[i][j] =
+                        BitmapFactory.decodeResource(
+                                activity.getResources(),
+                                resIdx[i][j]);
+            }
+        }
     }
 
     /* additional needed methods/variables: see the original card class for details
